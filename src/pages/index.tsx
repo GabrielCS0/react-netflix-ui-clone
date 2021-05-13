5e1b1d4360eaac81a6e02df4401915dc54eeeded
Gabriel <gabrielcemp@gmail.com>
2021-05-12 21:46:45 -0300

    Typescript & ESLint

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Home () {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <p>Hello</p>
+    </div>
+  )
+}
+// initial code
+// begin
